ab373552270f746c81b78e470b5cf099917546a7
MMH-Git <nozimica@dcc.uchile.com>
2012-11-01 17:34:58 -0300

    Removed Template_IT commented usage.
    Removed also htmlHeadLibs old use. Now it's just included by Twig.
    Renamed template variable INGRESAR to LOGIN_MSG.
    Renamed template variable AFTER to ACTION.
    Created template variable PROJ_ROOT.
    Deleted old block functions used by Template_IT.

--- src/_View.php
+++ src/_View.php
@@ -1,5 +1,4 @@
 <?php
-//require_once "HTML/Template/IT.php";
 require_once "vendor/Twig/lib/Twig/Autoloader.php";
 Twig_Autoloader::register();
 
@@ -49,20 +48,20 @@ class HtmlViewBase extends ViewBase implements iBasicView {
 
     public function __construct() {
         $this->mainView = new HtmlView();
-    }   
-    
+    }
+
     protected function _completeParsing($token = 'CONTENIDO') {
         $this->mainView->setVariable($token, $this->contentView->toHtml());
         $this->mainView->show();
-    }   
+    }
 
     public function nfw_printMessage($msg) { }
 
     public function nfw_loginAction($resultArr, $afterLogin)
     {
         $this->contentView = new HtmlView('_ingresar');
-        $this->contentView->setVariable('INGRESAR', $resultArr);
-        $this->contentView->setVariable('AFTER', $afterLogin);
+        $this->contentView->setVariable('LOGIN_MSG', $resultArr);
+        $this->contentView->setVariable('ACTION', $afterLogin);
         $this->_completeParsing();
     }
 
@@ -84,59 +83,25 @@ class HtmlView {
     protected $replacements;
 
     public function __construct($action = null) {
-        /*
-        $this->templateObj = new HTML_Template_IT($this->templPath);
-        if (is_null($action)) {
-            $this->templateObj->loadTemplatefile("_main.tpl.html");
-            $this->_loadHeadLibs();
-        } else {
-            $this->templateObj->loadTemplatefile("$action.tpl.html");
-        }
-        */
-
         $this->twigObj = new Twig_Environment(new Twig_Loader_Filesystem('templates'), array('autoescape' => false));
         $this->replacements = array();
         if (is_null($action)) {
             $this->templateObjTwig = $this->twigObj->loadTemplate("_main.tpl.html");
-            $this->_loadHeadLibs();
+            $this->setVariable('PROJ_ROOT', '/ViewerV4');
         } else {
             $this->templateObjTwig = $this->twigObj->loadTemplate("$action.tpl.html");
         }
     }
 
-    private function _loadHeadLibs() {
-        if ( ($libsStr = file_get_contents($this->templPath . '/_htmlHeadLibs.tpl.html')) !== false) {
-            //$this->templateObj->setVariable('HEADLIBS', $libsStr);
-            $this->replacements['HEADLIBS'] = $libsStr;
-        } else {
-            //$this->templateObj->setVariable('HEADLIBS', '');
-        }
-    }
-
     public function setVariable($varName, $varValue) {
-        //$this->templateObj->setVariable($varName, $varValue);
         $this->replacements[$varName] = $varValue;
     }
 
-    public function beginBlock($block) {
-        //$this->templateObj->setCurrentBlock($block);
-    }
-
-    public function endBlock($block) {
-        //$this->templateObj->parseCurrentBlock($block);
-    }
-
-    public function parseBlock($block) {
-        //$this->templateObj->parse($block);
-    }
-
     public function show($msg=null) {
-        //$this->templateObj->show();
-        echo $this->templateObjTwig->render($this->replacements);
+        echo $this->toHtml();
     }
 
     public function toHtml() {
-        //return $this->templateObj->get();
         return $this->templateObjTwig->render($this->replacements);
     }
 }
