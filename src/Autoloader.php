8ebad2dfa71c56e7424b465fca73854747ff2f24
nozimica @ chincol <nozimica@gmail.com>
2013-11-26 18:28:25 -0300

    Autoloader loads new View classes.

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -8,7 +8,13 @@ class Autoloader
         require_once __DIR__ . '/Controller.php';
         require_once __DIR__ . '/Model/FactoryModel.php';
         require_once __DIR__ . '/Model/Model.php';
-        require_once __DIR__ . '/View/View.php';
+        require_once __DIR__ . '/View/FactoryView.php';
+        require_once __DIR__ . '/View/iBasicView.php';
+        require_once __DIR__ . '/View/ViewBase.php';
+        require_once __DIR__ . '/View/HtmlViewBase.php';
+        require_once __DIR__ . '/View/HtmlView.php';
+        require_once __DIR__ . '/View/AjaxViewBase.php';
+        require_once __DIR__ . '/View/JsonViewBase.php';
         require_once __DIR__ . '/Authentication/FactoryAuth.php';
 
         require_once __DIR__ . '/../vendor/autoload.php';
