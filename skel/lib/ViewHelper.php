76f5dfb59c49cfb667ffc63a8b50902ee35b1758
nozimica @ chincol <nozimica@gmail.com>
2014-01-07 15:45:54 -0300

    Simple formatting changes.

--- skel/lib/ViewHelper.php
+++ skel/lib/ViewHelper.php
@@ -1,13 +1,17 @@
 <?php
 
-class AjaxViewBuilder extends AjaxViewBase {
-    public function creaTrabajoAction($resultArr) {
+class AjaxViewBuilder extends AjaxViewBase
+{
+    public function creaTrabajoAction($resultArr)
+    {
         $this->show($resultArr);
     }
 }
 
-class HtmlViewBuilder extends HtmlViewBase {
-    public function inicioAction($resultArr) {
+class HtmlViewBuilder extends HtmlViewBase
+{
+    public function inicioAction($resultArr)
+    {
         $this->buildPage('inicio', array('START' => $resultArr));
     }
 }
